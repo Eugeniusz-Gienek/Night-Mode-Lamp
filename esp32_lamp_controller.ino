588964e4d128830f5d40f71da1e46b929919407d
Eugeniusz <eugene@sky.community>
2024-11-19 08:47:07 +0100

    Update esp32_lamp_controller.ino

--- esp32_lamp_controller.ino
+++ esp32_lamp_controller.ino
@@ -9,6 +9,9 @@
 #define I2C_FREQ 400000
 #define RELAY_PIN 4
 
+#define START_HOUR 8
+#define FINISH_HOUR 22
+
 RTC_DS3231 rtc;
 
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
@@ -16,8 +19,9 @@ char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursd
 
 TwoWire I2C_1 = TwoWire(0);
 
+int just_started=1;
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   I2C_1.begin(I2C1_SDA1, I2C1_SCL1, I2C_FREQ);
   pinMode(RELAY_PIN, INPUT | OUTPUT_OPEN_DRAIN);
@@ -32,16 +36,29 @@ void setup() {
     Serial.println("RTC lost power, let's set the time!");
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
-  setupOTA("NightModeLampController", myWifiSSID, myWifiPASSWORD);
 }
 
 void loop() {
-  #ifdef defined(ESP32_RTOS) && defined(ESP32)
-  #else // If you do not use FreeRTOS, you have to regulary call the handle method.
-    ArduinoOTA.handle();
-  #endif
   DateTime now = rtc.now();
-  if((now.hour() >= 8) && (now.hour() < 22)) {
+  if(just_started == 1) {
+    just_started = 0;
+    if((now.hour() >= START_HOUR) && (now.hour() < FINISH_HOUR)) {
+      delay(500);
+      digitalWrite(RELAY_PIN, LOW);
+    } else {
+      delay(500);
+      digitalWrite(RELAY_PIN, HIGH);
+    }
+    delay(500);
+    setupOTA("NoghtModeLampController", myWifiSSID, myWifiPASSWORD);
+  }
+  else {
+    #ifdef defined(ESP32_RTOS) && defined(ESP32)
+    #else // If you do not use FreeRTOS, you have to regulary call the handle method.
+      ArduinoOTA.handle();
+    #endif
+  }
+  if((now.hour() >= START_HOUR) && (now.hour() < FINISH_HOUR)) {
     delay(500);
     digitalWrite(RELAY_PIN, LOW);
   } else {
@@ -49,37 +66,4 @@ void loop() {
     digitalWrite(RELAY_PIN, HIGH);
   }
   delay(500);
-    /*
-    Serial.print(now.year(), DEC);
-    Serial.print('/');
-    Serial.print(now.month(), DEC);
-    Serial.print('/');
-    Serial.print(now.day(), DEC);
-    Serial.print(" (");
-    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-    Serial.print(") ");
-    Serial.print(now.hour(), DEC);
-    Serial.print(':');
-    Serial.print(now.minute(), DEC);
-    Serial.print(':');
-    Serial.print(now.second(), DEC);
-    Serial.println();
-
-    Serial.print(" since midnight 1/1/1970 = ");
-    Serial.print(now.unixtime());
-    Serial.print("s = ");
-    Serial.print(now.unixtime() / 86400L);
-    Serial.println("d");
-
-    Serial.print("Temperature: ");
-    Serial.print(rtc.getTemperature());
-    Serial.println(" C");
-
-    Serial.println();
-    */
-
-    /*delay(1000);
-    digitalWrite(RELAY_PIN, LOW);
-    delay(1000);
-    digitalWrite(RELAY_PIN, HIGH);*/
 }
